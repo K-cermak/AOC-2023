282288e48d64782c4f52ea90d2d9dafd6f898099
EnplatedSyncer <karel4cermak@seznam.cz>
2023-12-10 02:03:14 +0100

    added day 5 part 2

--- 05-2.py
+++ 05-2.py
@@ -6,7 +6,6 @@ def main() -> None:
         data_edit = [line.rstrip() for line in data]
 
     seeds_str = data_edit[0].split(": ")[1].split(" ")
-    min_count = None
     
     # remove new line and first index from data
     data.pop(0)
@@ -16,15 +15,32 @@ def main() -> None:
         else:
             data[i] = data[i].rstrip()
 
+    intervals = []
+    min_count = None
 
     for i in range(0, len(seeds_str), 2):
         seed_start = int(seeds_str[i])
         seed_end = seed_start + int(seeds_str[i + 1])
+        intervals.append((seed_start, seed_end))
 
-        for seed in range(seed_start, seed_end):
-            ret = process(data, seed)
+    for index, interval in enumerate(intervals):
+        start, end = interval
+
+        ret, limit = process(data, start)
+        if min_count is None or ret < min_count:
+            min_count = ret
+
+        if start + limit >= end:
+            ret, _ = process(data, end)
             if min_count is None or ret < min_count:
                 min_count = ret
+        else:
+            ret, _ = process(data, start + limit)
+            if min_count is None or ret < min_count:
+                min_count = ret
+
+            intervals[index] = (start, start + limit)
+            intervals.append((start + limit + 1, end))
 
     print("-----")
     print(min_count)
@@ -32,6 +48,8 @@ def main() -> None:
 
 def process(data, seed):
     block_process = False
+    max_limit = 9999999999999999999 # scuffed
+    maybe_limit = 9999999999999999999 # scuffed
 
     for line in data:
         if ":" not in line:
@@ -42,12 +60,24 @@ def process(data, seed):
                 three = int(nums[2])
 
                 if two <= seed < (two + three):
+                    new_limit = (two + three) - seed
+                    if new_limit < max_limit:
+                        max_limit = new_limit
+
                     block_process = True
                     seed = first + seed - two
+
+
+                elif seed < two and (two - seed) < maybe_limit:
+                    maybe_limit = two - seed - 1
+
         else:
+            if not block_process and maybe_limit < max_limit:
+                max_limit = maybe_limit
+
             block_process = False
 
-    return seed
+    return seed, max_limit
 
 if __name__ == "__main__":
     main()
